4cfff7447006c040a59983846077004d1902769e
Salma Samir <salmashoair@gmail.com>
2020-01-19 18:38:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -86,23 +86,3 @@ ask.addEventListener('click', () => {
     message.textContent = 'Listening ...';
 });
 
-
-
-
-
-
-
-
-// function twtPost(txtID, URL) {
-//   var txt = document.getElementById(txtID).textContent;
-//   window.open('https://twitter.com/share?text=' + txt + '&url=' + URL, "_blank");
-// }
-
-// function fbPost(URL) {
-//   window.open('https://www.facebook.com/sharer/sharer.php?u=' + URL, "_blank");
-// }
-
-// function whatsAppShare(txtID, URL) {
-//   var txt = document.getElementById(txtID).textContent;
-//   window.location.href = "whatsapp://send?text=" + txt + " -- Link: " + URL;
-// }
